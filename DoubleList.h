5f898f22ad2d3cb96f1d0c3dce690d658213bc07
Álvaro Berdote <35387108+SeXde@users.noreply.github.com>
2020-09-16 13:31:24 +0200

    Update DoubleList.h

--- DoubleList.h
+++ DoubleList.h
@@ -10,12 +10,19 @@ class DoubleList
 {
 	Node * head = nullptr;
 	Node * tail = nullptr;
+	int getLength();
+
 public:
+
 	virtual ~DoubleList();
 	bool isEmpty();
 	void addNode(int data);
+	void addNodeAfter(int data);
+	void addNodeBefore(int data);
+	void addNodeRandom(int data);
 	void deleteNode(int data);
 	void displayList();
 };
+
 
 
